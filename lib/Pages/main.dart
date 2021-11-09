b71828369348e101508c6f811a3dc5aa2ae2bcc4
Zohaib <muhammad.zohaib9613@hotmail.com>
2021-11-09 13:53:46 +0500

    add appbar with search

--- lib/Pages/main.dart
+++ lib/Pages/main.dart
@@ -6,8 +6,50 @@ class Main extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0.5,
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          icon: const Icon(
+            Icons.account_circle,
+            color: Colors.black,
+          ),
+          onPressed: () {},
+        ),
+        actions: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  color: const Color(0xffe9ecef),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                width: MediaQuery.of(context).size.width * 0.75,
+                child: const TextField(
+                  decoration: InputDecoration(
+                    isDense: true,
+                    filled: true,
+                    fillColor: Colors.transparent,
+                    hintText: 'Search',
+                    enabledBorder: InputBorder.none,
+                    focusedBorder: InputBorder.none,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          IconButton(
+            icon: const Icon(
+              Icons.search,
+              color: Colors.black,
+            ),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: const Center(
         child: Text("First Flight"),
       ),
     );
