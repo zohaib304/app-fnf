db26e7bc0f7ea11bed4868a21330c56d5c2793fd
Zohaib <muhammad.zohaib9613@hotmail.com>
2021-11-20 22:15:40 +0500

    added firestore pagination

--- lib/Pages/home.dart
+++ lib/Pages/home.dart
@@ -1,11 +1,11 @@
-import 'package:android_app_fnf/Models/product.dart';
 import 'package:android_app_fnf/Models/product_argumets.dart';
-import 'package:android_app_fnf/Services/products.dart';
 import 'package:android_app_fnf/Widgets/carousel_slider_main.dart';
 import 'package:android_app_fnf/Widgets/sign_in_sheet.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:paginate_firestore/paginate_firestore.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
 import 'package:share_plus/share_plus.dart';
@@ -15,7 +15,6 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final products = Provider.of<Products>(context, listen: false);
     final firebaseUser = context.watch<User?>();
     return Scaffold(
       backgroundColor: const Color(0xffF5F6F8),
@@ -113,156 +112,150 @@ class Home extends StatelessWidget {
             ),
 
             const SizedBox(height: 10),
-            StreamBuilder<List<Product>>(
-              stream: products.getProducts(),
-              builder: (context, snapshot) {
-                if (snapshot.hasData) {
-                  return GridView.builder(
-                    shrinkWrap: true,
-                    physics: const NeverScrollableScrollPhysics(),
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2,
-                      childAspectRatio: 4 / 6.2,
-                    ),
-                    itemCount: snapshot.data!.length,
-                    itemBuilder: (context, index) {
-                      return GestureDetector(
-                        onTap: () {
-                          Navigator.pushNamed(context, '/product-detail',
-                              arguments: ProductArguments(
-                                snapshot.data![index].id,
-                                snapshot.data![index].name,
-                                snapshot.data![index].price,
-                                snapshot.data![index].supplierId,
-                                snapshot.data![index].images[0],
-                              ));
-                        },
-                        child: Container(
-                          margin: const EdgeInsets.all(2),
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(5),
-                            border: Border.all(
-                              color: Colors.black12,
-                            ),
-                          ),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              CarouselSlider(
-                                options: CarouselOptions(
-                                  height: 180,
-                                  enableInfiniteScroll: false,
-                                  viewportFraction: 1,
-                                ),
-                                items: snapshot.data![index].images.map((i) {
-                                  return Builder(
-                                    builder: (BuildContext context) {
-                                      return SizedBox(
-                                        width:
-                                            MediaQuery.of(context).size.width,
-                                        child: ClipRRect(
-                                          borderRadius:
-                                              const BorderRadius.vertical(
-                                            top: Radius.circular(5),
-                                          ),
-                                          child: CachedNetworkImage(
-                                            imageUrl: i,
-                                            fit: BoxFit.cover,
-                                          ),
-                                        ),
-                                      );
-                                    },
-                                  );
-                                }).toList(),
-                              ),
-                              Container(
-                                padding: const EdgeInsets.all(10),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      snapshot.data![index].name,
-                                      maxLines: 2,
-                                      style: const TextStyle(
-                                        fontSize: 12,
-                                        color: Colors.black54,
-                                      ),
-                                    ),
-                                    // price
-                                    Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceBetween,
-                                      children: [
-                                        Text(
-                                          'Rs. ${snapshot.data![index].price.toInt()}',
-                                          style: const TextStyle(
-                                            fontSize: 18,
-                                            fontWeight: FontWeight.w500,
-                                            color: Colors.black87,
-                                          ),
-                                        ),
-                                        IconButton(
-                                          onPressed: () {
-                                            Share.share(
-                                              snapshot.data![index].name,
-                                            );
-                                          },
-                                          icon: const Icon(
-                                            Icons.share_outlined,
-                                            size: 22,
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                    Row(
-                                      children: [
-                                        Container(
-                                          padding: const EdgeInsets.symmetric(
-                                            horizontal: 8,
-                                            vertical: 2,
-                                          ),
-                                          decoration: BoxDecoration(
-                                            color: Colors.green,
-                                            borderRadius:
-                                                BorderRadius.circular(10),
-                                          ),
-                                          child: const Text(
-                                            "4.8",
-                                            style: TextStyle(
-                                              fontSize: 12,
-                                              color: Colors.white,
-                                            ),
-                                          ),
-                                        ),
-                                        const SizedBox(
-                                          width: 2,
-                                        ),
-                                        const Text("Ratings"),
-                                      ],
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ],
+            PaginateFirestore(
+              onEmpty: const Text("Something went wrong."),
+              itemBuilderType:
+                  PaginateBuilderType.gridView, //Change types accordingly
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                childAspectRatio: 4 / 5.9,
+              ),
+              itemBuilder: (context, documentSnapshots, index) {
+                final data = documentSnapshots[index].data() as Map?;
+                return Column(
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.pushNamed(context, '/product-detail',
+                            arguments: ProductArguments(
+                              documentSnapshots[index].id,
+                              data!["productName"],
+                              double.parse(data["price"]),
+                              data["supplier_id"],
+                              data['image'][0],
+                            ));
+                      },
+                      child: Container(
+                        margin: const EdgeInsets.all(2),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(5),
+                          border: Border.all(
+                            color: Colors.black12,
                           ),
                         ),
-                      );
-                    },
-                  );
-                }
-                if (snapshot.hasError) {
-                  return const Text('Something went wrong');
-                }
-
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const Center(
-                    child: CircularProgressIndicator(),
-                  );
-                }
-                return const Text("Loading...");
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            CarouselSlider(
+                              options: CarouselOptions(
+                                height: 180,
+                                enableInfiniteScroll: false,
+                                viewportFraction: 1,
+                              ),
+                              items: data!['image'].map<Widget>((i) {
+                                return Builder(
+                                  builder: (BuildContext context) {
+                                    return SizedBox(
+                                      width: MediaQuery.of(context).size.width,
+                                      child: ClipRRect(
+                                        borderRadius:
+                                            const BorderRadius.vertical(
+                                          top: Radius.circular(5),
+                                        ),
+                                        child: CachedNetworkImage(
+                                          imageUrl: i,
+                                          fit: BoxFit.cover,
+                                        ),
+                                      ),
+                                    );
+                                  },
+                                );
+                              }).toList(),
+                            ),
+                            Container(
+                              padding: const EdgeInsets.all(10),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    data['productName'],
+                                    maxLines: 1,
+                                    style: const TextStyle(
+                                      fontSize: 12,
+                                      color: Colors.black54,
+                                    ),
+                                  ),
+                                  // price
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text(
+                                        'Rs. ${data['price']}',
+                                        style: const TextStyle(
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.w500,
+                                          color: Colors.black87,
+                                        ),
+                                      ),
+                                      IconButton(
+                                        onPressed: () {
+                                          Share.share(
+                                            data['productName'],
+                                          );
+                                        },
+                                        icon: const Icon(
+                                          Icons.share_outlined,
+                                          size: 22,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  Row(
+                                    children: [
+                                      Container(
+                                        padding: const EdgeInsets.symmetric(
+                                          horizontal: 8,
+                                          vertical: 2,
+                                        ),
+                                        decoration: BoxDecoration(
+                                          color: Colors.green,
+                                          borderRadius:
+                                              BorderRadius.circular(10),
+                                        ),
+                                        child: const Text(
+                                          "4.8",
+                                          style: TextStyle(
+                                            fontSize: 12,
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                      ),
+                                      const SizedBox(
+                                        width: 2,
+                                      ),
+                                      const Text("Ratings"),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                );
               },
+              // orderBy is compulsory to enable pagination
+              query: FirebaseFirestore.instance
+                  .collection('products')
+                  .orderBy('productName'),
+              // to fetch real-time data
+              isLive: true,
             ),
           ],
         ),
--- lib/Services/products.dart
+++ lib/Services/products.dart
@@ -1,19 +1,28 @@
+import 'dart:developer';
+
 import 'package:android_app_fnf/Models/product.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class Products with ChangeNotifier {
+  // last document snapshot
+  late DocumentSnapshot lastDocumentSnapshot;
+  int documentLimit = 2;
+  bool _hasNext = true;
+  bool _isFetchingProducts = false;
 
   // get all products from firebase database and store it in a list
   Stream<List<Product>> getProducts() {
     final collectionReference =
         FirebaseFirestore.instance.collection('products');
-    final snapshots = collectionReference.snapshots();
+    final snapshots = collectionReference.limit(documentLimit).snapshots();
 
     return snapshots.map(
       (snapshot) => (snapshot.docs.map(
         (snapshot) {
           final data = snapshot.data();
+          // last document snapshot
+          lastDocumentSnapshot = snapshot;
           return Product(
             id: snapshot.id,
             name: data["productName"],
@@ -26,6 +35,29 @@ class Products with ChangeNotifier {
       ).toList()),
     );
   }
+
+  // // get more products from firebase database and store it in a list
+  // Stream<List<Product>> getMoreProducts(String lastDoc) {
+  //   final collectionReference =
+  //       FirebaseFirestore.instance.collection('products');
+  //   final snapshots = collectionReference.startAfterDocument(lastDoc).limit(2).snapshots();
+
+  //   return snapshots.map(
+  //     (snapshot) => (snapshot.docs.map(
+  //       (snapshot) {
+  //         final data = snapshot.data();
+  //         return Product(
+  //           id: snapshot.id,
+  //           name: data["productName"],
+  //           price: double.parse(data["price"]),
+  //           description: data['productDescription'],
+  //           supplierId: data['supplier_id'],
+  //           images: data['image'] as List<dynamic>,
+  //         );
+  //       },
+  //     ).toList()),
+  //   );
+  // }
 
   // get product by id
   Stream<Product> getProductById(docId) {
