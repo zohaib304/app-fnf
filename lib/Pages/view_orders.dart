9256f78d51899cb12d55ef3633f0f975a171677e
Zohaib <muhammad.zohaib9613@hotmail.com>
2021-12-11 05:47:37 +0500

    added btm appbar

--- lib/Pages/view_orders.dart
+++ lib/Pages/view_orders.dart
@@ -1,16 +1,42 @@
 import 'package:flutter/material.dart';
 
 class ViewOrders extends StatelessWidget {
-  const ViewOrders({ Key? key }) : super(key: key);
+  const ViewOrders({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('View Orders'),
-      ),
-      body: const Center(
-        child: Text('View Orders'),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          title: const Text('Orders'),
+          bottom: const TabBar(
+            unselectedLabelStyle: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: Colors.black38,
+            ),
+            tabs: [
+              Tab(
+                text: 'Pending',
+              ),
+              Tab(
+                text: 'Completed',
+              ),
+              Tab(
+                text: 'Cancelled',
+              ),
+            ],
+          ),
+        ),
+        body: const TabBarView(
+          children: [
+            Icon(Icons.directions_car),
+            Icon(Icons.directions_transit),
+            Icon(Icons.directions_bike),
+          ],
+        ),
       ),
     );
   }
