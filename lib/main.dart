64b020c4e2f0b389b6fd71dd8fb9ceee6f7adb5c
Zohaib <muhammad.zohaib9613@hotmail.com>
2022-02-13 04:21:03 +0500

    added flutterfire_cli

--- lib/main.dart
+++ lib/main.dart
@@ -19,13 +19,16 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
+import 'firebase_options.dart';
 
 // local imports
 import 'Pages/home.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
